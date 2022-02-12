6fc89909ed636aeca6a9f3126ae1797369bdd3d6
Im_Cynx <silentninj4.7@gmail.com>
2022-02-12 23:24:49 +0100

    + New S13 file + Added bloom + Added reflection + Added lights (with reflection) + New floor item

--- src/hooks/useThree.tsx
+++ src/hooks/useThree.tsx
@@ -1,9 +1,32 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
+import { ReflectorForSSRPass } from 'three/examples/jsm/objects/ReflectorForSSRPass.js';
+import { SSRPass } from 'three/examples/jsm/postprocessing/SSRPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 
 
 export default function useThree() {
+
+    const params = {
+      enableSSR: true,
+      autoRotate: true,
+      otherMeshes: true,
+      groundReflector: true,
+      exposure: 0.1,
+      bloomStrength: 0.4,
+      bloomThreshold: 0,
+      bloomRadius: 0.7
+    };
+
+    const selects: THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>[] | null = [];
 
 
     useEffect(() => {
@@ -13,13 +36,13 @@ export default function useThree() {
         const canvas = document.querySelector('.home-three-object') as HTMLElement;
         const scene = new THREE.Scene();
         const loader = new GLTFLoader();
-        const distance = 5;
+        const distance = 6;
         
-        loader.load( process.env.PUBLIC_URL + "../models/s13-2.glb", ( gltf ) => {
+        loader.load( process.env.PUBLIC_URL + "../models/s13.glb", ( gltf ) => {
           const file = gltf;
-          file.scene.rotateY(0.8)
-          file.scene.translateX(-1);
-          // file.scene.translateZ(1);
+          file.scene.rotateY(0.8);
+          file.scene.translateX(1);
+          file.scene.translateZ(-1);
           scene.add( file.scene );
         
         }, undefined, function ( error ) {
@@ -28,46 +51,71 @@ export default function useThree() {
         
         } );
 
-
-
+  
         // ------------------- Object -------------------
-        const geometry = new THREE.BoxGeometry( 1, 1, 1);
+        // const cube = new THREE.BoxGeometry( 1, 1, 1);
+        // const cubeMaterial = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0.1, metalness: 0 } );
+				// const cubesMaterial = new THREE.Mesh( cube, cubeMaterial );
+        // cubesMaterial.position.y = 0.75;
+        // scene.add(cubesMaterial);
         
        
 
 
 
         // ------------------- Material -------------------
-        const material = new THREE.MeshStandardMaterial();
-        material.metalness = 0.025;
-        material.roughness = 0.5;
-        material.color = new THREE.Color(0x2194ce);
+        const geoFloor = new THREE.BoxGeometry( 8, 0.05, 8 );
+				const matStdFloor = new THREE.MeshStandardMaterial( { color: 0x000000, roughness: 0, metalness: 0 } );
+				const mshStdFloor = new THREE.Mesh( geoFloor, matStdFloor );
+				scene.add( mshStdFloor );
         
-
-
-
-        // ------------------- Mesh -------------------
-        const cube = new THREE.Mesh(geometry,material);
-        // scene.add(cube);
-
 
         
         
         // ------------------- Lights -------------------
-        const fill = new THREE.PointLight(0xffffff, 1);
-        fill.position.set(-2, 5, 4);
-        scene.add(fill);
 
-        const key = new THREE.PointLight(0xffffff, 1.2);
-        key.position.set(1, 1, 1);
-        scene.add(key);
+        RectAreaLightUniformsLib.init();
 
-        const back = new THREE.PointLight(0xffffff, 0.2);
-        back.position.set(3, -1, -8);
-        scene.add(back);
+        const rectLight1 = new THREE.RectAreaLight( 0xffffff, 1, 1.5, 3 );
+				rectLight1.position.set( -4, 1, -5 );
+        rectLight1.rotateY(3.4);
+				scene.add( rectLight1 );
+        scene.add( new RectAreaLightHelper( rectLight1 ) );
+
+        const rectLight2 = new THREE.RectAreaLight( 0xffffff, 1, 1.5, 3 );
+				rectLight2.position.set( 0, 1, -5 );
+        rectLight2.rotateY(3.15);
+				scene.add( rectLight2 );
+        scene.add( new RectAreaLightHelper( rectLight2 ) );
+
+        const rectLight3 = new THREE.RectAreaLight( 0xffffff, 1, 1.5, 3 );
+				rectLight3.position.set( 4, 1, -5 );
+        rectLight3.rotateY(2.9);
+				scene.add( rectLight3 );
+        scene.add( new RectAreaLightHelper( rectLight3 ) );
+
+        const light = new THREE.AmbientLight(0xffffff, 0.2);
+        scene.add(light);
+
+        const dirlight = new THREE.DirectionalLight(0xffffff, 0.5);
+        dirlight.position.set(-4, 15, 2);
+        dirlight.target.position.set(-1,0,0);
+        scene.add(dirlight);
+        scene.add(dirlight.target);
 
 
-
+        const geometry = new THREE.PlaneBufferGeometry( 1, 1 );
+        const groundReflector = new ReflectorForSSRPass( geometry, {
+          clipBias: 0.0003,
+          textureWidth: window.innerWidth,
+          textureHeight: window.innerHeight,
+          color: 0x888888,
+          useDepthTexture: true,
+        } );
+        groundReflector.material.depthWrite = false;
+        groundReflector.rotation.x = - Math.PI / 2;
+        groundReflector.visible = false;
+        scene.add( groundReflector );
 
 
    
@@ -81,11 +129,11 @@ export default function useThree() {
 
 
         // ------------------- Camera -------------------
-        var camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+        var camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 100);
         camera.position.set(0,0, distance);
         scene.add(camera);
 
-        const cursor = { x : 0 , y : -0.2, z : distance}
+        const cursor = { x : 0 , y : -0.7, z : distance}
         document.onmousemove = (event) => {
           cursor.x = (event.clientX / window.innerWidth) - 0.5;
           cursor.y = ((event.clientY / window.innerHeight) - 1.2);
@@ -99,10 +147,38 @@ export default function useThree() {
         const renderer = new THREE.WebGLRenderer({
             canvas: canvas,
             alpha: true,
+            // antialias: true,
         })
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
         
+
+        const composer = new EffectComposer( renderer );
+        const ssrPass = new SSRPass( {
+          renderer,
+          scene,
+          camera,
+          width: window.innerWidth,
+          height: window.innerHeight,
+          groundReflector: params.groundReflector ? groundReflector : null,
+          selects: params.groundReflector ? selects : null
+        } );
+
+        const renderScene = new RenderPass( scene, camera );
+
+        const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+        bloomPass.threshold = params.bloomThreshold;
+        bloomPass.strength = params.bloomStrength;
+        bloomPass.radius = params.bloomRadius;
+
+        // composer.addPass( renderScene );
+        
+        composer.addPass( ssrPass );
+        composer.addPass( new ShaderPass( GammaCorrectionShader ) );
+        composer.addPass( bloomPass );
+
+        ssrPass.thickness = 0.018;
+        ssrPass.isInfiniteThick = false;
 
 
 
@@ -113,17 +189,19 @@ export default function useThree() {
       
             // const elapsedTime = clock.getElapsedTime()
         
-            const cameraX = cursor.x * 5;
-            const cameraY = - cursor.y * 1.5;
+            const cameraX = cursor.x * 3;
+            const cameraY = - cursor.y;
             const cameraZ = cursor.z
 
-            camera.rotation.x += (-cameraY / 4 - camera.rotation.x) / 15;
-            camera.rotation.y += (cameraX / 4 - camera.rotation.y) / 15;
+            camera.rotation.x += (-cameraY / 4 + 0.1 - camera.rotation.x) / 15;
+            camera.rotation.y += (cameraX / 6 - camera.rotation.y) / 15;
             camera.position.x += (cameraX - camera.position.x) / 15;
-            camera.position.y += (cameraY - camera.position.y) / 15;
+            camera.position.y += (cameraY + 0.5 - camera.position.y) / 15;
             camera.position.z += (cameraZ - camera.position.z) / 15;
 
-            renderer.render(scene, camera);
+            // renderer.render(scene, camera);
+
+            composer.render();
 
             window.requestAnimationFrame(tick);
         }
